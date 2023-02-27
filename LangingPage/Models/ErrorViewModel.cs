cf7427249babe3d9cf04cf957f3c067a09e5d5f1
David Sian <Davidsian804@gmail.com>
2023-02-27 15:36:19 -0600

    Agregar archivos de proyecto.

---
+++ LangingPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LangingPage.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
